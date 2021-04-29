07a6f1314185a83cf682ea53f85ac27d2435ea4d
handy-reza <55019786+hyrezaalf@users.noreply.github.com>
2021-04-29 16:26:33 +0700

    rz

--- Admin/home.php
+++ Admin/home.php
@@ -1,5 +1,4 @@
-<center><br><br><br><br><br><br><br><br>
-<img src="" width="200px height="200px" /> <br>
+<center><br><br><br>
+<img src="./assets/images/developer.png" width="500px height="500px" /> <br>
 <font Size="6" face="Helvetica">GEEK DASHBOARD</font> <br>
-<font Size="6">Universitas Muhammadiyah Malang</font>
 </center>
