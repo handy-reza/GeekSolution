745250710c98800690cc65f2443b311e944f3823
handy-reza <55019786+hyrezaalf@users.noreply.github.com>
2021-04-29 21:24:41 +0700

    config db

--- Admin/team/config.php
+++ Admin/team/config.php
@@ -1,6 +1,6 @@
 <?php
 //koneksi ke database mysql,
-$koneksi = mysqli_connect("localhost","root","reza157354","akademik");
+$koneksi = mysqli_connect("us-cdbr-east-03.cleardb.com","bbe50cb499db89","18f0f5ab","akademik");
 
 //cek jika koneksi ke mysql gagal, maka akan tampil pesan berikut
 if (mysqli_connect_errno()){
