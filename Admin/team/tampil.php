87684e570c4d1a2e54cf9b08fa8730ac0dbd29e6
handy-reza <55019786+hyrezaalf@users.noreply.github.com>
2021-04-29 17:24:32 +0700

    Admin fix

--- Admin/team/tampil.php
+++ Admin/team/tampil.php
@@ -7,7 +7,7 @@ include('config.php');
 	<div class="container" style="margin-top:20px">
 		<center><font size="6">Data Mahasiswa</font></center>
 		<hr>
-		<a href="#"><button class="btn btn-dark right">Tambah Data</button></a>
+		<a href="index.php?page=tambah_mhs"><button class="btn btn-dark right">Tambah Data</button></a>
 		<div class="table-responsive">
 		<table class="table table-striped jambo_table bulk_action">
 			<thead>
